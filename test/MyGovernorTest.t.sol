d2ff6b3257da291c0de33d1d91d6a59e2668c37e
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-06-08 14:00:57 +0530

    setup function and simple test

--- test/MyGovernorTest.t.sol
+++ test/MyGovernorTest.t.sol
@@ -12,6 +12,40 @@ contract MyGovernorTest is Test {
     TimeLock timelock;
     MyGovernor governor;
     Box box;
+    uint256 public constant INITIAL_SUPPLY = 100 ether;
+    address public USER = makeAddr("user");
+    address[] proposers;
+    address[] executors;
+    uint256 public constant MIN_DELAY = 3600; // 1 hour - after a vote passes, you have 1 hour before you can enact
 
-    function setUp() public {}
+    uint256 public constant QUORUM_PERCENTAGE = 4; // Need 4% of voters to pass
+    uint256 public constant VOTING_PERIOD = 50400; // This is how long voting lasts
+    uint256 public constant VOTING_DELAY = 1; // How many blocks till a proposal vote becomes active
+
+    function setUp() public {
+        token = new GovToken();
+        token.mint(USER, INITIAL_SUPPLY);
+
+        vm.startPrank(USER);
+        token.delegate(USER);
+        timelock = new TimeLock(MIN_DELAY, proposers, executors);
+        governor = new MyGovernor(token, timelock);
+
+        bytes32 proposerRole = timelock.PROPOSER_ROLE();
+        bytes32 executorRole = timelock.EXECUTOR_ROLE();
+        bytes32 adminRole = timelock.TIMELOCK_ADMIN_ROLE();
+
+        timelock.grantRole(proposerRole, address(governor));
+        timelock.grantRole(executorRole, address(0));
+        timelock.revokeRole(adminRole, USER);
+        vm.stopPrank();
+
+        box = new Box();
+        box.transferOwnership(address(timelock));
+    }
+
+    function testCantUpdateBoxWithoutGovernance() public {
+        vm.expectRevert();
+        box.store(1);
+    }
 }
